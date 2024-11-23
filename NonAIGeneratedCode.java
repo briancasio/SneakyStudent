ed3a30dfb2c7a8ea64ed6a4c5d14d1e55233216b
briancasio <113497684+briancasio@users.noreply.github.com>
2024-11-23 05:27:14 -0700

    Update NonAIGeneratedCode.java

--- NonAIGeneratedCode.java
+++ NonAIGeneratedCode.java
@@ -2,17 +2,17 @@ import java.util.Scanner;
 
 public class NonAIGeneratedCode{
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
         
         System.out.print("Enter the number of elements in the array: ");
-        int size = sc.nextInt();
+        int size = input.nextInt();
         
         int[] numbers = new int[size];
         
         System.out.println("Enter " + size + " integers:");
         for (int i = 0; i < size; i++) {
-            numbers[i] = sc.nextInt();
+            numbers[i] = input.nextInt();
         }
         
         System.out.println("You entered the following numbers:");
